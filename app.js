8e90cc65fc165a2e49cf7617db28258fa3fdd90c
Akrambek1702 <Akrambek1702@gmail.com>
2021-11-29 10:52:57 +0500

    counter

---
+++ app.js
@@ -0,0 +1,20 @@
+let count = 0;
+
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+
+btns.forEach(function (btn) {
+    btn.addEventListener('click', function(e) {
+        const styles = e.currentTarget.classList;
+        if(styles.contains('decrease')){
+            count --;
+        } else if(styles.contains('increase')){
+            count ++;
+        } else if(styles.contains('reset')){
+            count = 0;
+        } 
+        
+        value.textContent = count;
+    })
+})
